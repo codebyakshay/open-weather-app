e7d30cb4558ae283eb5f2f8d78489f38fcf662c3
codebyakshay <admin@codebyakshay.com>
2024-04-23 10:14:20 +0530

    finial commit
    this the the final commit of the project Implemented feature where weather icon dynamically change b/w day and night , please make sure to use your own API keys for testing !

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
-const apiKey = '4abe666d851d3d5cf8e8d6693f2ea568';
-const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-icon");
 
 // Global variable to store weather data
-
 let globalWeatherData = {};
 
 async function checkWeather(cityName) {
     try {
-        const response = await fetch(url + cityName + `&appid=${apiKey}`);
+        const response = await fetch(weatherUrl + cityName + `&appid=${apiKey}`);
         if (!response.ok) {
             throw new Error('Failed to fetch data');
         }
         const weather = await response.json();
         globalWeatherData = weather; // Store the fetched data globally
-
         updateUI(weather);
-        return weather.dt; // Return the timestamp if needed
+        return weather;
+
     } catch (error) {
         console.error("Error:", error);
         document.querySelector(".error").style.display = "block";
@@ -27,31 +26,46 @@ async function checkWeather(cityName) {
 }
 
 function updateUI(weather) {
+
+    var city = weather.name;
+    getLocalTime(city, weather);
     document.querySelector(".error").style.display = "none";
     document.querySelector(".city").innerHTML = weather.name;
     document.querySelector(".temp").innerHTML = Math.round(weather.main.temp) + `&deg;C`;
     document.querySelector(".humidity").innerHTML = weather.main.humidity + `%`;
     document.querySelector(".wind").innerHTML = weather.wind.speed + ` km/h`;
-
-    // Adjusting for timezone: weather.timezone gives offset in seconds from UTC
-    const localTime = new Date((weather.dt * 1000) + (weather.timezone * 1000)); // Convert to milliseconds
-
-    const hour = localTime.getHours(); // Get the local current hour in 24-hour format
-
-    // Determine if it's day or night
-    let partOfDay = 'day';
-    if (hour >= 19 || hour < 6) { // Use night image after 7 PM or before 6 AM
-        partOfDay = 'night';
-    }
-    
-    // Set the image path based on the weather type and part of the day
-    const weatherType = weather.weather[0].main;
-    let imagePath = `/images/images/${weatherType}/${partOfDay}.png`; // Updated image path setting
-
-    weatherIcon.src = imagePath;
     document.querySelector(".weather").style.display = "block";
-    console.log(weatherType);
+    
+    
 }
+
+async function getLocalTime(city, weather) {
+    const timeUrl = `https://api.ipgeolocation.io/timezone?apiKey=&location=`;
+    try {
+        
+        const response = await fetch(timeUrl + city);
+        const timeData = await response.json();
+        const timeInHours = parseInt(timeData.time_24.split(':')[0], 10);
+        const imageUrl = 'images/'
+        const weatherType = weather.weather[0].main;
+        const weatherIcon = document.querySelector(".weather-icon");
+        // Checking the time range and logging the appropriate message
+        if (timeInHours >= 6 && timeInHours <= 17) {
+            weatherIcon.src = ``;
+            weatherIcon.src = `${imageUrl}${weatherType}/day.png`;
+            weatherIcon.setAttribute("title", weatherType + " day");
+
+        } else {
+            weatherIcon.src = ``;
+            weatherIcon.src = `${imageUrl}${weatherType}/night.png`;
+            weatherIcon.setAttribute("title", weatherType + " night");
+        }
+
+    } catch (error) {
+        console.error('Failed to fetch time data:', error);
+    }
+}
+
 
 // Event listener for the search button click
 searchBtn.addEventListener("click", function () {
@@ -60,7 +74,7 @@ searchBtn.addEventListener("click", function () {
 
 // Event listener for keypress in the search box
 searchBox.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {  // Check if the key pressed is the Enter key
+    if (event.key === "Enter") {
         triggerSearch();
     }
 });
